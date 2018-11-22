4205766312f953f7ca8a642f26135aa93c9b545b
JNRuan <jnruan@gmail.com>
2018-11-22 11:27:19 +1000

    Version 0.6.4c, for workshop.

--- src/windows/about.py
+++ src/windows/about.py
@@ -6,7 +6,7 @@ from utilities import resource_path
 
 REPO_LINK = 'https://github.com/CoEDL/hermes'
 ONLINE_DOCS = 'https://github.com/CoEDL/hermes/tree/master/docs'
-VERSION = '0.6.4b'
+VERSION = '0.6.4c'
 
 
 class AboutWindow(QDialog):
