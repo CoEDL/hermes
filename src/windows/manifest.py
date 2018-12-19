beb6304aab83a8841056b01d36e6be880003500b
JNRuan <jnruan@gmail.com>
2018-12-20 09:31:05 +1000

    Language Manifest now asks for Confirmation of details, drawing from project details.

--- src/windows/manifest.py
+++ src/windows/manifest.py
@@ -8,28 +8,31 @@ class ManifestWindow(QDialog):
                  parent: QMainWindow,
                  data: ConverterData):
         super().__init__(parent)
+        self.parent = parent
         self.data = data
         self.layout = QGridLayout()
         self.widgets = Box()
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle('Enter Manifest Details')
+        self.setWindowTitle('Confirm Manifest Details')
         self.setMinimumWidth(300)
         transcription_language_label = QLabel('Transcription Language:')
         self.layout.addWidget(transcription_language_label, 0, 0, 1, 1)
         self.widgets.transcription_language_field = QLineEdit()
+        self.widgets.transcription_language_field.setText(self.parent.session.data_transcription_language)
         self.layout.addWidget(self.widgets.transcription_language_field, 0, 1, 1, 7)
 
         translation_language_label = QLabel('Translation Language:')
         self.layout.addWidget(translation_language_label, 1, 0, 1, 1)
         self.widgets.translation_language_field = QLineEdit()
-        self.widgets.translation_language_field
+        self.widgets.translation_language_field.setText(self.parent.session.data_translation_language)
         self.layout.addWidget(self.widgets.translation_language_field, 1, 1, 1, 7)
 
         author_name_label = QLabel('Author Name:')
         self.layout.addWidget(author_name_label, 2, 0, 1, 1)
         self.widgets.author_name_field = QLineEdit()
+        self.widgets.author_name_field.setText(self.parent.session.data_author)
         self.layout.addWidget(self.widgets.author_name_field, 2, 1, 1, 7)
 
         save_button = QPushButton('Save')
