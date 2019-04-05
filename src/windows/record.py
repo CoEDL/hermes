c6f08f4e9dcea86fc54e16b88ebacf52e70e36cf
JNRuan <jnruan@gmail.com>
2019-04-05 14:19:41 +1000

    Enabled Preview audio button iff there is audio to preview. Swapped position of save/cancel buttons.

--- src/windows/record.py
+++ src/windows/record.py
@@ -26,15 +26,17 @@ class RecordWindow(QDialog):
         self.layout = QGridLayout()
         self.record_button = QPushButton()
         self.recording = False
+        self.output = None
         self.init_ui()
         self.recorder = SimpleAudioRecorder(self.data,
                                             self.transcription,
                                             self.settings)
-        self.output = None
 
     def init_ui(self) -> None:
         self.setWindowTitle('Record Audio')
         self.setMinimumWidth(200)
+
+        # Header
         instruction_text = f'<html>Click button below to start recording.<br/>' \
                            f'Click the button again to stop recording.<br/>'
         if self.transcription.transcription:
@@ -42,6 +44,8 @@ class RecordWindow(QDialog):
         instruction_label = QLabel(instruction_text)
         instruction_label.setAlignment(Qt.AlignCenter)
         self.layout.addWidget(instruction_label, 0, 0, 1, 9)
+
+        # Buttons
         self.record_button.setStyleSheet('QPushButton{border-radius: 50%;'
                                          '            height: 100px;'
                                          '            width: 100px;'
@@ -55,18 +59,26 @@ class RecordWindow(QDialog):
         self.record_button.clicked.connect(self.on_click_record)
         self.layout.addWidget(self.record_button, 1, 0, 1, 9)
         self.layout.setAlignment(self.record_button, Qt.AlignCenter)
-        cancel_button = QPushButton('Cancel')
-        cancel_button.clicked.connect(self.on_click_cancel)
-        self.layout.addWidget(cancel_button, 3, 6, 1, 1)
-        self.preview_button = QPushButton('Preview')
-        self.preview_button.clicked.connect(self.on_click_preview)
-        self.preview_button.setEnabled(False)
-        self.layout.addWidget(self.preview_button, 3, 7, 1, 1)
+
         save_button = QPushButton('Save')
         save_button.clicked.connect(self.on_click_save)
-        self.layout.addWidget(save_button, 3, 8, 1, 1)
+        self.layout.addWidget(save_button, 3, 6, 1, 1)
+
+        self.preview_button = QPushButton('Preview')
+        self.preview_button.clicked.connect(self.on_click_preview)
+        if self.transcription.sample and self.transcription.sample.get_sample_file_path():
+            self.output = self.transcription.sample.get_sample_file_path()
+        else:
+            self.preview_button.setEnabled(False)
+        self.layout.addWidget(self.preview_button, 3, 7, 1, 1)
+
+        cancel_button = QPushButton('Cancel')
+        cancel_button.clicked.connect(self.on_click_cancel)
+        self.layout.addWidget(cancel_button, 3, 8, 1, 1)
+
         self.layout.setSizeConstraint(QLayout.SetFixedSize)
         self.setLayout(self.layout)
+
         LOG_RECORD_WINDOW.debug("Record Window initialised.")
 
     def on_click_record(self) -> None:
@@ -114,7 +126,9 @@ class RecordWindow(QDialog):
         self.close()
 
     def on_click_preview(self) -> None:
+        print(self.output)
         if self.output:
+            print(self.output)
             mixer.init()
             sound = mixer.Sound(self.output)
             sound.play()
